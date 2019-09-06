00e8f4ca2c8c0654d1dff9ef91e16217d9168635
tulkasa <moiseenkorf@gmail.com>
2019-09-06 17:40:11 +0300

    JavaOutFormat solution

---
+++ hackerrank/JavaOutputFormatting/src/ru/tulkasa/hakerrank/JavaOutputFormatting/JavaOutFormat.java
@@ -0,0 +1,21 @@
+package ru.tulkasa.hakerrank.JavaOutputFormatting;
+
+import java.util.Scanner;
+
+public class JavaOutFormat {
+
+    /*
+        https://www.hackerrank.com/challenges/java-output-formatting/problem
+     */
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+        }
+        System.out.println("================================");
+
+    }
+}
