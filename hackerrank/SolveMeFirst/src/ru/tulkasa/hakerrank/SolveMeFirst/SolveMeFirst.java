4f94b2ca0af15d2b888161938df8bf22c6141e73
tulkasa <moiseenkorf@gmail.com>
2019-09-08 16:10:53 +0300

    SolveMeFirst Solution

---
+++ hackerrank/SolveMeFirst/src/ru/tulkasa/hakerrank/SolveMeFirst/SolveMeFirst.java
@@ -0,0 +1,27 @@
+package ru.tulkasa.hakerrank.SolveMeFirst;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/*
+https://www.hackerrank.com/challenges/solve-me-first/problem
+ */
+public class SolveMeFirst {
+
+    static int solveMeFirst(int a, int b) {
+        // Hint: Type return a+b; below
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
