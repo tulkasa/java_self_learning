8f2d91f8c08f3ec40b3a06eabfa3e9cbb9797ca9
tulkasa <moiseenkorf@gmail.com>
2019-09-08 14:06:55 +0300

    JavaIIDArray Solution

---
+++ hackerrank/JavaIIDArray/src/ru/tulkasa/hakerrank/JavaIIDArray/JavaIIDArray.java
@@ -0,0 +1,37 @@
+package ru.tulkasa.hakerrank.JavaIIDArray;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+/*
+https://www.hackerrank.com/challenges/java-2d-array/problem
+ */
+public class JavaIIDArray {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int maxSum = Integer.MIN_VALUE, sum = 0;
+        ;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if ((i + 2 < 6) && (j + 2) < 6) {
+                    sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j]
+                            + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    if (sum > maxSum)
+                        maxSum = sum;
+                }
+            }
+        }
+        System.out.println(maxSum);
+        in.close();
+    }
+}
