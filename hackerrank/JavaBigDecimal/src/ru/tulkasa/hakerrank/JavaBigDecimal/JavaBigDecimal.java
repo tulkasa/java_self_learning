e7988ba9a27aa4091991e42b3b1b8addd1d0a060
tulkasa <moiseenkorf@gmail.com>
2019-09-08 13:50:55 +0300

    JavaBigDecimal Solution

---
+++ hackerrank/JavaBigDecimal/src/ru/tulkasa/hakerrank/JavaBigDecimal/JavaBigDecimal.java
@@ -0,0 +1,29 @@
+package ru.tulkasa.hakerrank.JavaBigDecimal;
+import java.math.BigDecimal;
+import java.util.*;
+/*
+https://www.hackerrank.com/challenges/java-bigdecimal/problem
+ */
+public class JavaBigDecimal {
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+        Arrays.sort(s, 0, n, Collections.reverseOrder(new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                BigDecimal b1 = new BigDecimal(s1);
+                BigDecimal b2 = new BigDecimal(s2);
+                return b1.compareTo(b2);}   }   )   );
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
