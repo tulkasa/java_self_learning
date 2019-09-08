c29fe537fcd14fb4dbbfcf642d688efff30369d9
tulkasa <moiseenkorf@gmail.com>
2019-09-08 11:33:53 +0300

    JavaStaticIniBlock Solution

---
+++ hackerrank/JavaStaticIniBlock/src/ru/tulkasa/hakerrank/JavaStaticIniBlock/JavaStaticIniBlock.java
@@ -0,0 +1,35 @@
+package ru.tulkasa.hakerrank.JavaStaticIniBlock;
+
+/*
+https://www.hackerrank.com/challenges/java-static-initializer-block/problem
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+
+public class JavaStaticIniBlock {
+    static Scanner sc = new Scanner(System.in);
+    static int B = sc.nextInt();
+    static int H = sc.nextInt();
+    static boolean flag;
+    static {
+        try {
+            if (B > 0 && H > 0) {
+                flag = true;
+            } else {
+                throw new Exception("Breadth and height must be positive");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
