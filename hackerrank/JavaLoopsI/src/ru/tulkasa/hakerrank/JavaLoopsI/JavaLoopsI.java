4b4955482e9359a695adc0ab141672ff53d84983
tulkasa <moiseenkorf@gmail.com>
2019-09-08 11:12:17 +0300

    JavaLoopsI Solution

---
+++ hackerrank/JavaLoopsI/src/ru/tulkasa/hakerrank/JavaLoopsI/JavaLoopsI.java
@@ -0,0 +1,32 @@
+package ru.tulkasa.hakerrank.JavaLoopsI;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/*
+https://www.hackerrank.com/challenges/java-loops-i/problem
+ */
+public class JavaLoopsI {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N>=2 && N<=20) {
+            for (int i=1; i<=10; i++) {
+                int result = N*i ;
+                System.out.println(N + " x " + i + " = " + result);
+            }
+        }
+
+
+        scanner.close();
+    }
+
+}
