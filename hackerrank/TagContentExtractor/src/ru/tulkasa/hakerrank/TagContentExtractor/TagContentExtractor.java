519ccafce9d258e8aa07469f2410f62e9c37062d
tulkasa <moiseenkorf@gmail.com>
2019-09-08 13:47:34 +0300

    TagContentExtractor Solution

---
+++ hackerrank/TagContentExtractor/src/ru/tulkasa/hakerrank/TagContentExtractor/TagContentExtractor.java
@@ -0,0 +1,34 @@
+package ru.tulkasa.hakerrank.TagContentExtractor;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/*
+https://www.hackerrank.com/challenges/tag-content-extractor/problem
+ */
+public class TagContentExtractor {
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String line = in.nextLine();
+            String regex = "<(.+)>([^<>]+)(</\\1>)";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(line);
+            int counter = 0;
+            while (m.find()) {
+                if (m.group(2).length() != 0) {
+                    System.out.println(m.group(2));
+                    counter++;
+                }
+            }
+            if (counter == 0)
+                System.out.println("None");
+            testCases--;
+        }
+        in.close();
+    }
+}
