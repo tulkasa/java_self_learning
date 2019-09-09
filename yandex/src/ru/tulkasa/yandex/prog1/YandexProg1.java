a595279de9208996c07f9c3c4407599f6841f9db
tulkasa <moiseenkorf@gmail.com>
2019-09-09 22:14:54 +0300

    YandexProg Solution

--- yandex/src/ru/tulkasa/yandex/prog1/YandexProg1.java
+++ yandex/src/ru/tulkasa/yandex/prog1/YandexProg1.java
@@ -1,4 +1,20 @@
 package ru.tulkasa.yandex.prog1;
-
+import java.io.*;
 public class YandexProg1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        String J = r.readLine();
+        String S = r.readLine();
+
+        int result = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            char ch = S.charAt(i);
+            if (J.indexOf(ch) >= 0) {
+                ++result;
+            }
+        }
+
+        System.out.println(result);
+    }
 }
